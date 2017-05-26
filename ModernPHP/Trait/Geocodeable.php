2a4f453d8718d1e93438e33f95303eb4ca7b23d9
songyichao <songyichao74@gmail.com>
2017-05-27 00:46:16 +0800

    fix

---
+++ ModernPHP/Trait/Geocodeable.php
@@ -0,0 +1,45 @@
+<?php
+
+trait Geocodeable
+{
+    protected $address;
+
+    protected $geocoder;
+
+    protected $geocoder_result;
+
+    public function setGeocoder(\Geocoder\GeocoderInterface $geocoder)
+    {
+        $this->geocoder = $geocoder;
+    }
+
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    public function getLatitude()
+    {
+        if (isset($this->geocoder_result) === false) {
+            $this->geocoderAddress();
+        }
+
+        return $this->geocoder_result->getLatitude();
+    }
+
+    public function getLongitude()
+    {
+        if (isset($this->geocoder_result) === false) {
+            $this->geocoderAddress();
+        }
+
+        return $this->geocoder_result->getLongitude();
+    }
+
+    protected function geocoderAddress()
+    {
+        $this->geocoder_result = $this->geocoder->geocoder($this->address);
+
+        return true;
+    }
+}
