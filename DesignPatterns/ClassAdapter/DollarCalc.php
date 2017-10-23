476e2b734f933fed4de40567f4a5a97e8f48edcb
songyichao <songyichao74@gmail.com>
2017-10-24 00:06:37 +0800

    fix classadapter

---
+++ DesignPatterns/ClassAdapter/DollarCalc.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * description
+ *
+ * @category   Learn
+ * @package    PSR
+ * @subpackage Documentation\API
+ * @author     yichaosong  <songyichao@gmail.com>
+ * @license    GPL https://songyichao.com
+ * @link       https://songyichao.com
+ */
+
+namespace DesignPatterns\ClassAdapter;
+
+
+class DollarCalc
+{
+    private $dollar;
+    private $product;
+    private $service;
+    public $rate = 1;
+
+    public function requestCalc($product_now, $service_now)
+    {
+        $this->product = $product_now;
+        $this->service = $service_now;
+        $this->dollar = $this->product + $this->service;
+
+        return $this->requestTotal;
+    }
+
+    public function requestTotal()
+    {
+        $this->dollar *= $this->rate;
+
+        return $this->dollar;
+    }
+}
